e68eef997aef84997f437aee763244a854387da7
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-05-23 10:41:08 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n;j>0;j--)         {             printf("%d ",j);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
+        for(j=n;j>0;j--)
         {
-            if(j==i||i==(n-j+1))
-            printf("x");
-            else
-            printf("0");
+            printf("%d ",j);
         }
         printf("
 ");
