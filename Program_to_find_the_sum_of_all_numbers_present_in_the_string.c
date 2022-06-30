3ba69994232f3a48da85bfe6f3589422776b6556
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-06-30 18:22:32 +0530

    #include<stdio.h> int main() {     int i,sum=0;     char s[100];     scanf("%[^ ]s",&s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]>='0'&&s[i]<='9')         {             sum+=(s[i]-'0');         }     }     printf("%d",sum);     return 0; }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,sum=0;
+    char s[100];
+    scanf("%[^
+]s",&s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]>='0'&&s[i]<='9')
+        {
+            sum+=(s[i]-'0');
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}
