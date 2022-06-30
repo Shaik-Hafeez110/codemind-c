ed8dfff161f026aabdac4bf2b412c719c9165896
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-06-30 19:17:20 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int a[n],b[n],i,j,k;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         b[i]=pow(a[i],2);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(b[i]>b[j])             {                 k=b[i];                 b[i]=b[j];                 b[j]=k;             }         }     }     for(i=0;i<n;i++)     printf("%d ",b[i]);     return 0;
    }

---
+++ _Squares_of_a_Sorted_Array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],b[n],i,j,k;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        b[i]=pow(a[i],2);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(b[i]>b[j])
+            {
+                k=b[i];
+                b[i]=b[j];
+                b[j]=k;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    printf("%d ",b[i]);
+    return 0;
+    
+}
