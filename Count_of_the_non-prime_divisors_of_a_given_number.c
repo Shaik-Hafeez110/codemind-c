dcc4179900920c77975f33a375d1e4b48ae5c811
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-07-02 20:39:00 +0530

    #include<stdio.h> int fun(int); int fun(int a) {     int i,s=0,d=0;     if(a==1)     return 1;     for(i=1;i<=a;i++)     {         if(a%i==0)         s++;     }     if(s>2)     return 1;     else     return 0;
    }
    int main()
    {
        int n,i,c=0;
        scanf("%d",&n);
        for(i=1;i<=n;i++)
        {
            if(n%i==0)
             {
               if(fun(i))
               c++;
             }
        }
        printf("%d",c);
    }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
-int main()
+int fun(int);
+int fun(int a)
 {
-    int a,i,j,n;
-    int t=0,l,k=0;
-    scanf("%d",&a);
+    int i,s=0,d=0;
+    if(a==1)
+    return 1;
     for(i=1;i<=a;i++)
     {
         if(a%i==0)
-        {l=0;
-            for(j=1;j<=i;j++)
-            {
-                if(i%j==0)
-                l++;
-            }
-            if(l==2)
-            {t++;}
-            k++;
-        }
+        s++;
     }
-    printf("%d",k-t);
+    if(s>2)
+    return 1;
+    else
     return 0;
+    
+}
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+         {
+           if(fun(i))
+           c++;
+         }
+    }
+    printf("%d",c);
 }
