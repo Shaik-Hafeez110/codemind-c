8d67bdb5ce496cc2616ccf5ea47142bad7912459
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-07-02 20:52:29 +0530

    #include<stdio.h> #include<string.h> int main() {     char a[100];     int i,wc=0;     scanf("%[^ ]s",a);     for(i=0;a[i]!=NULL;i++)     {         if(a[i]==32)         wc++;     }     printf("%d",wc+1); }

---
+++ words_in_a_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i,wc=0;
+    scanf("%[^
+]s",a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]==32)
+        wc++;
+    }
+    printf("%d",wc+1);
+}
