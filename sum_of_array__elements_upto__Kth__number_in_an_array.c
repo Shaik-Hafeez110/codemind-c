1a9ee57392760c55882398c39f8285e94188d8bd
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-07-03 14:08:35 +0530

    #include<stdio.h> int main() {     int i,j,n,a[200],c=0,m;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&m);     for(i=0;i<n;i++)     {         if(a[i]<=m)         c+=a[i];     }     printf("%d",c); }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,a[200],c=0,m;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=m)
+        c+=a[i];
+    }
+    printf("%d",c);
+}
