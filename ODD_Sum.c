68668c5d3fbdfc31ff9b6a6640294e8501d025d4
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-07-11 11:11:56 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,o=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[n]);         if(a[n]%2!=0)         o+=a[n];     }     printf("%d",o);     return 0; }

--- ODD_Sum.c
+++ ODD_Sum.c
@@ -3,13 +3,13 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],i,c=0;
-    for (i=0;i<n;i++)
+    int a[n],i,o=0;
+    for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-        if (a[i]%2!=0)
-        c+=a[i];
+        scanf("%d",&a[n]);
+        if(a[n]%2!=0)
+        o+=a[n];
     }
-    printf("%d",c);
+    printf("%d",o);
     return 0;
 }
