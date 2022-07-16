a2032a521ec50f8f9b15c6e227f7890324e1d97e
Hafeezjack <97437298+Hafeezjack@users.noreply.github.com>
2022-07-16 09:44:13 +0530

    #include<stdio.h> #include<math.h> int main() { 	int n,d,s=0,c,f=0,temp; 	scanf("%d",&n); 	temp=n; 	while(n) 	{ 		d=n%10; 		f++; 		n/=10; 	} 	n=temp; 	while(temp) 	{ 		d=temp%10; 		s+=pow(d,f); 		f--; 		temp/=10; 	} 	if(s==n) 	printf("True"); 	else 	printf("False");
    }

--- Disarium_number.c
+++ Disarium_number.c
@@ -2,20 +2,27 @@
 #include<math.h>
 int main()
 {
-    int n,z=0,r,t,d;
-    scanf("%d",&n);
-    t=n;
-    d=(int)log10(n)+1;
-    while(n>0)
-    {
-        r=n%10;
-        z=z+ceil(pow(r,d));
-        n=n/10;
-        d--;
-    }
-    if(z==t)
-        printf("True");
-    else
-       printf("False");
-    return 0;   
+	int n,d,s=0,c,f=0,temp;
+	scanf("%d",&n);
+	temp=n;
+	while(n)
+	{
+		d=n%10;
+		f++;
+		n/=10;
+	}
+	n=temp;
+	while(temp)
+	{
+		d=temp%10;
+		s+=pow(d,f);
+		f--;
+		temp/=10;
+	}
+	if(s==n)
+	printf("True");
+	else
+	printf("False");
+
+	
 }
